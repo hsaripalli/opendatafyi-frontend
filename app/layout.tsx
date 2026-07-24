8a85e7f517173521cb4ef25fa654130c185e27f2
Hari Saripalli <gaara.awp@gmail.com>
2026-07-24 09:54:06 -0600

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import favicon16 from "../opendata-fyi-original-dots-logo-pack/favicon-16.png";
 import favicon32 from "../opendata-fyi-original-dots-logo-pack/favicon-32.png";
@@ -49,6 +50,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
